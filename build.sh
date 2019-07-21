be22191296afcf3e0db6b7f3538b0c4a0b1bdb89
Oleg Sidorov <me@whitebox.io>
2019-07-21 18:24:30 +0200

    Moved all build logic to a stand-2lone script
    Signed-off-by: Oleg Sidorov <me@whitebox.io>

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -e
+
+docker run -ti -v $(pwd):/miktex/work -e MIKTEX_GID=$(id -g) -e MIKTEX_UID=$(id -u) miktex/miktex /bin/bash -c "mpm --update-db && pdflatex sample.tex"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add sample.pdf
+git commit -m "[skip travis] Automatic PDF build from ${TRAVIS_COMMIT}"
+git pull --rebase
+git remote add origin-push "https://${GH_TOKEN}@github.com/icanhazbroccoli/latex-sandbox.git"
+git push --quiet --set-upstream origin-push HEAD
