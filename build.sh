8605c229c4f0c1cd1073337e0529a79d8ad4fa32
Oleg Sidorov <me@whitebox.io>
2019-07-21 22:37:15 +0200

    Refactored build script
    Signed-off-by: Oleg Sidorov <me@whitebox.io>

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 set -e
 
-docker run -ti -v $(pwd):/miktex/work -e MIKTEX_GID=$(id -g) -e MIKTEX_UID=$(id -u) miktex/miktex /bin/bash -c "mpm --update-db && pdflatex sample.tex"
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git checkout master
-git add sample.pdf
+USEBRANCH=master
+BUILDSRC=sample.tex
+BUILDRES=sample.pdf
+GITEMAIL="travis@travis-ci.org"
+GITNAME="Travis CI"
+GITSRC="github.com/icanhazbroccoli/latex-sandbox.git"
+
+git checkout "${USEBRANCH}"
+docker run -ti -v $(pwd):/miktex/work -e MIKTEX_GID=$(id -g) -e MIKTEX_UID=$(id -u) miktex/miktex /bin/bash -c "mpm --update-db && pdflatex ${BUILDSRC}"
+git config --global user.email "${GITEMAIL}"
+git config --global user.name "${GITNAME}"
+git add "${BUILDRES}"
 git commit -m "[skip travis] Automatic PDF build from ${TRAVIS_COMMIT}"
 git pull --rebase
-git remote set-url origin "https://${GITHUB_TOKEN}@github.com/icanhazbroccoli/latex-sandbox.git"
-git push origin master
+git remote set-url origin "https://${GITHUB_TOKEN}@${GITSRC}"
+git push origin "${USEBRANCH}"
